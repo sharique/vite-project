ba67dae1a1cb44b99847b4362a0dcb0782961fd0
Sharique Farooqui <safknw@gmail.com>
2023-12-27 02:22:49 +0100

    Adding storybook and first component.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+
+import Slider from './components/Slider.vue';
 </script>
 
 <template>
   <div>
+    <Slider>test</Slider>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
@@ -11,7 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  
 </template>
 
 <style scoped>
@@ -21,9 +23,11 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
---
+++ src/components/Slider.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+import {ref} from 'vue';
+const value = ref(0)
+</script>
+
+<template>
+    <h3>
+        Slider
+    </h3>
+    <input type="range" v-model="value" />
+    <div class="test">{{ value }}</div>
+</template>
